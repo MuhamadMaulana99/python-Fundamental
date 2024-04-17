8ee98dd4fee0867a4cead3ec1e2dc76217806cf9
muhamad maulana <maulanamuhamad593@gmail.com>
2024-04-17 13:01:40 +0700

    test

---
+++ conditional.py
@@ -0,0 +1,24 @@
+# ifElse 
+nama = input('Masukan nama: ' )
+
+if nama == 'maulana' :
+    print('kamu ganteng banget maulana')
+else:
+    print(f'kamu gak ganteng karna kamu {nama}')
+    
+print(f'program selesai')
+
+# elif 
+'================================================================='
+nama = input('Masukan nama: ' )
+
+if nama == 'maulana' :
+    print('kamu ganteng banget maulana')
+elif nama == 'sonia':
+    print(f'kamu mantan tersayang karna kamu {nama}')
+elif nama == 'leni':
+    print(f'kamu mantan tersayang juga karna kamu {nama}')
+else:
+    print(f'kamu bukan siapa" {nama}')
+    
+print(f'program selesai')
---
+++ perulangan.py
@@ -0,0 +1,21 @@
+angka = [1,2,3,4,5]
+
+for i in angka :
+    print(f'angka: {i}')
+'========================='
+angka2 = range(4)
+
+for i in angka2 :
+    print(f'angka2: {i}')
+    
+'========================='
+dataStr = 'Muhamad Maulana'
+# bisa mengulang string gilaa bet 
+for i in dataStr :
+    print(f'dataStr: {i}')
+'========================='
+angka3 = 0
+while angka3 < 5:
+    angka3 += 1 # angka3 = angka3 + 1
+    print(f'datass: {angka3}')
+    
